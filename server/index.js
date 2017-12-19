f1b6e66bb6acef86557f41fb7ce3c07ba47c3f9d
Quy.Tran <quy.tran@smoovapp.com>
2017-12-19 08:24:55 +0700

    Add logic code

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-module.exports = function Server(x){
-    var path    = x.path,
+module.exports = function Server(x) {
+    var path = x.path,
         express = x.express,
-        morgan  = x.morgan,
-        helmet  = x.helmet;
-        
-    const start = function(){
-        var port        = x.config.env.PORT || 8080;
-        
+        morgan = x.morgan,
+        helmet = x.helmet;
+
+    const start = function() {
+        var port = x.config.env.PORT || 8080;
+
         const app = express();
         app.use(morgan('dev'));
         app.use(express.static(path.join(__dirname, '../public')));
@@ -14,12 +14,12 @@ module.exports = function Server(x){
         app.use('/truck', x.routes.truck);
         app.use('/user', x.routes.user);
 
-        const server = app.listen(x.config.env.PORT, function()        {
-            console.info(x.util.format('The ' + x.config.APP_NAME + ' is running on port %d', port));    
+        const server = app.listen(x.config.env.PORT, function() {
+            console.info(x.util.format('The ' + x.config.APP_NAME + ' is running on port %d', port));
         });
     };
 
     return {
-        start : start
+        start: start
     }
 };
--- x.js
+++ x.js
@@ -1,33 +1,33 @@
-module.exports = function X(config){
-    var x       = {},
-        env     = require('node-env-file'),
-        util    = require('util'),
-        winston = require('winston');      
+module.exports = function X(config) {
+    var x = {},
+        env = require('node-env-file'),
+        util = require('util'),
+        winston = require('winston');
 
-    x.util      = util;
-    x.express   = require('express');
-    x.morgan    = require('morgan');
-    x.helmet    = require('helmet');    
-    x.path      = require('path');  
-
+    x.util = util;
+    x.express = require('express');
+    x.morgan = require('morgan');
+    x.helmet = require('helmet');
+    x.path = require('path');
+    x.mongoose = require('mongoose');
     var x;
 
-        /**
+    /**
      *
      * @private
      * @static
      * @property x
      * @type {Object}
      */
-    if(typeof config === 'undefined') config = {};
-    
-    if (!process.env.PORT) { env('.env');}    
+    if (typeof config === 'undefined') config = {};
 
-    x.config            = config;
-    x.config.env        = process.env;  
+    if (!process.env.PORT) { env('.env'); }
 
-    x.apis              = require('./api')(x);
-    x.routes            = require('./routing')(x).routes;
+    x.config = config;
+    x.config.env = process.env;
+
+    x.apis = require('./api')(x);
+    x.routes = require('./routing')(x).routes;
 
 
     return x;
